33a52826d18f622fc5d21135af5e17f9a279caa1
Snezhana <alot3602@gmail.com>
2023-01-26 01:29:24 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+ 
+ int num1 = 9012;
+Console.WriteLine(GetSum(num1));
+
+ int GetSum(int num)
+{
+    int res = 0;
+    for(int i = 1; i <= num; i++)
+    {
+        res = res + num % 10;
+        num = num / 10;
+    }
+    return res;
+}
